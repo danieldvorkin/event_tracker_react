79987165c7e5025508e79c8121441179cfbeca32
Daniel Dvorkin <daniel.dvorkin@ptaginc.com>
2019-10-11 00:36:47 -0400

    small thing

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 import Editor from './Editor';
-import './stylesheets/App.css';
 
 // Stateless function which will contain all relevant routes
 const App = () => (
